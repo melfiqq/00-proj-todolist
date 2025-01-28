8d4f92a35678e5c48a9f616c11cc9a27fa576374
Maryie <mashenka.zinkevich0245@gmail.com>
2025-01-28 16:29:11 +0200

    proj-start-01.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Todolist } from './Todolist';
+import { TaskType } from './Todolist';
+
+const tasks1: Array<TaskType> = [
+  {id: 1, title: "CSS&HTML", isDone: true},
+  {id: 2, title: "React", isDone: true},
+  {id: 3, title: "PHP", isDone: false}
+]
+
+const tasks2: Array<TaskType> = [
+  {id: 1, title: "Harry Potter.", isDone: false},
+  {id: 2, title: "SashkaKakashka", isDone: true},
+  {id: 3, title: "Eat. Pray. Love.", isDone: false}
+]
 
 function App() {
   return (
     <div className="App">
-      <Todolist />
+      <Todolist title="What to learn: " tasks={tasks1} />
+      <Todolist title="What to watch: " tasks={tasks2}/>
     </div>
   );
 }
-
-function Todolist() {
-  return <div>TODOLIST</div>
-} 
 
 export default App;
